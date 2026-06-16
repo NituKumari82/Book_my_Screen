3cbbb2c151c43d340695fff18b5c6b0daa244759
Nitu Kumari <nitukumarigupta82@gmail.com>
2026-06-16 16:24:51 +0530

    added history

--- bms-frontend/src/App.jsx
+++ bms-frontend/src/App.jsx
@@ -1,30 +1,69 @@
-import { Route, Routes } from 'react-router-dom'
-import Header from './components/shared/Header'
-import Footer from './components/shared/Footer'
+import { Navigate, Outlet, Route, Routes, useMatch } from "react-router-dom";
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import Profile from "./pages/Profile";
+import SeatLayout from "./pages/SeatLayout";
+import Checkout from "./pages/Checkout";
+import { Toaster } from "react-hot-toast";
+import { useLoadUser } from "./hooks/useLoadUser";
+import FullScreenLoader from "./components/shared/FullScreenLoader";
+import { useAuth } from "./context/AuthContext";
 
-function App() {
-  return (
-    <>
-      <div className="flex flex-col h-screen"> {/* Fixed typo: classaName -> className */}
-        
-        {/* ✅ Header sits outside Routes so it renders on every page */}
-        <Header /> 
 
-        <main className="flex-grow">
-          {/* ✅ All <Route> tags must live inside the <Routes> wrapper */}
-          <Routes>
-            <Route path='/' element={<h1>Home Page</h1>}/>
-            <Route path='/profil/:id' element={<h1>Profile Page</h1>}/>
-            <Route path='/movies' element={<h1>Movie Page</h1>}/>
-          </Routes>
-        </main>
-
-        {/* ✅ Footer sits outside Routes so it renders on every page */}
-        <Footer />
-
-      </div>
-    </>
-  )
+const PrivateRoute = () => {
+  const { auth }  = useAuth();
+  return auth ? <Outlet /> : <Navigate to="/" replace />;
 }
 
-export default App
+function  App() {
+  
+  const { isLoading } = useLoadUser();
+  
+
+   // Hide header/footer only on seat layout page
+  const isSeatLayoutPage = useMatch(
+    "/movies/:movieId/:movieName/:state/theater/:theaterId/show/:showId/seat-layout"
+  );
+
+  const isCheckoutPage = useMatch("/shows/:showId/:state/checkout");
+
+  if(isLoading) {
+    return <FullScreenLoader />
+  }
+
+  return (
+    <>
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          style : {
+            fontSize : "14px",
+          }
+        }}
+      />
+      <div className="flex flex-col min-h-screen">
+        {!isSeatLayoutPage && !isCheckoutPage && <Header />}
+        <main className="flex-grow">
+          <Routes>
+            {/* Define your routes here */}
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:state/:movieName/:id/ticket" element={<MovieDetails />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/movies/:movieId/:movieName/:state/theater/:theaterId/show/:showId/seat-layout" element={<SeatLayout />} />
+              <Route path="/profile/:id/:tab" element={<Profile />} />
+              </Route>
+            <Route path="/shows/:showId/:state/checkout" element={<Checkout />} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </main>
+        {!isSeatLayoutPage && !isCheckoutPage && <Footer />}
+      </div>
+    </>
+  );
+}
+
+export default App;
